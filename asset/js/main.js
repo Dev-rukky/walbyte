be1e82fc656eb857cc87034d5044cdc5b5fc97ce
Rukky <abrukevwe@gmail.com>
2023-09-10 18:33:47 +0100

    omo

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,3 +6,21 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("nav-open");
 });
+
+
+// const expertExpand = document.getElementsByClassName("carousel__contents");
+
+// expertExpand.addEventListener("click", () => {
+//     expertExpand.classList.toggle(".carousel__contents-expand");
+// });
+
+// Get all elements with the class "carousel__contents"
+const expertExpandElements = document.getElementsByClassName("carousel__item");
+
+// Add a click event listener to each element
+for (const element of expertExpandElements) {
+  element.addEventListener("click", () => {
+    // Toggle the "carousel__contents-expand" class on the clicked element
+    element.classList.toggle("carousel__contents-expand");
+  });
+}
